412e78b501d08be7b8a9ebe21cae78f2cae04d3a
Veru <43966969+VeruAmari@users.noreply.github.com>
2023-11-22 19:50:49 -0300

    Add barebones pseudocode for dropdown-menu

--- src/image-carousel.js
+++ src/image-carousel.js
@@ -3,3 +3,9 @@ export default function carousell(selector) {
     console.log(element);
   });
 }
+
+/* Pseudocode:
+
+Need to create a reusable JS function that moves focus to a specific IMG in the carousell.
+This function can then be invoked to both JUMP to a certain image when clicking on some "little circles UI" AND to simply "go to next/previous" image when clicking some "next/previous arrow UI element"
+*/
